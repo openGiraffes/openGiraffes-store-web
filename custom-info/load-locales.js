ed4152150b1308693b7666b898472a0c886f2779
LiarOnce <qq1062787843@hotmail.com>
2021-06-17 13:25:29 +0000

    Deploying to gh-pages from  @ 6333b70929b1d7053f95430a7a774b4353aad1ff 🚀

---
+++ custom-info/load-locales.js
@@ -0,0 +1,23 @@
+var lang = new Lang();
+
+// If you want to add more languages, please add a line here
+lang.dynamic("en", "assets/locales/en.json");
+lang.dynamic("zh-CN", "assets/locales/zh-CN.json");
+
+lang.init({
+    defaultLang: "en",
+    currentLang: 'zh-CN',
+    cookie: {
+        name: 'langCookie',
+        expiry: 365,
+        path: '/'
+    },
+    allowCookieOverride: true
+});
+
+function changeLocales(locale) {
+    window.lang.change(locale);
+    // window.reloadData();
+    window.location.reload();
+    return false;
+}
